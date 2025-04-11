c42c92566c2af589ce7061a17ee16acc00d4575e
Sara-Mosteller <sara.m.mosteller@gmail.com>
2025-04-11 20:58:47 +0100

    Add files via upload

---
+++ 7_plotly_dash_app.py
@@ -0,0 +1,4 @@
+import wget
+wget.download("https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DS0321EN-SkillsNetwork/datasets/spacex_launch_dash.csv")
+
+wget.download("https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DS0321EN-SkillsNetwork/labs/module_3/spacex_dash_app.py")
